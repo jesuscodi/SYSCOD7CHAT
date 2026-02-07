c04c6aaa93ff187a99c5ed52ff80fbbc4fd262b9
Jesus <jesusgp72021@gmail.com>
2026-02-07 13:07:58 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,42 @@
-import { auth } from "./firebase.js";
-import { 
+import { auth, db } from "./firebase.js";
+import {
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
+import {
+  doc,
+  setDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 
-document.getElementById("btnLogin").addEventListener("click", async () => {
-  try {
-    await signInWithEmailAndPassword(auth, email.value, password.value);
-    alert("Bienvenido");
-    window.location = "dashboard.html";
-  } catch (error) {
-    alert(error.message);
-  }
-});
+if (document.getElementById("btnLogin")) {
 
-document.getElementById("btnRegistro").addEventListener("click", async () => {
-  try {
-    await createUserWithEmailAndPassword(auth, email.value, password.value);
-    alert("Cuenta creada");
-  } catch (error) {
-    alert(error.message);
-  }
-});
+  document.getElementById("btnLogin").addEventListener("click", async () => {
+    try {
+      await signInWithEmailAndPassword(auth, email.value, password.value);
+      window.location = "dashboard.html";
+    } catch (error) {
+      alert(error.message);
+    }
+  });
+
+  document.getElementById("btnRegistro").addEventListener("click", async () => {
+    try {
+      const user = await createUserWithEmailAndPassword(auth, email.value, password.value);
+
+      // guardar rol
+      await setDoc(doc(db, "usuarios", user.user.uid), {
+        email: email.value,
+        rol: "maestro" // luego puedes cambiar a admin
+      });
+
+      alert("Usuario creado");
+    } catch (error) {
+      alert(error.message);
+    }
+  });
+
+}
